a4659dad949e521dff1fcc9ec6bab45db85777fa
Kay <kaytech429@gmail.com>
2026-06-24 18:17:55 +0000

    Added the animation of the atom before the app loads to the lgin screen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:susu/screens/splashsreen.dart';
 
-class Load extends StatelessWidget{
+class Load extends StatefulWidget{
   const Load({super.key});
+
+  @override
+  State<Load> createState() => _LoadState();
+}
+
+class _LoadState extends State<Load> {
+
+  @override
+  void initState() {
+    super.initState();
+    _navigateAfterDelay();
+  }
+
+  void _navigateAfterDelay() {
+    Future.delayed(const Duration(seconds: 1), () {
+      if (!mounted) return;
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const SplashScreen()),
+      );
+    });
+  }
+
 
   @override
   Widget build(BuildContext context){
