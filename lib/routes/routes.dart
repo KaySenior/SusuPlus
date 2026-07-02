43c6e3a4e4a6dd5093bdc54570ed41096def6b56
Kay <kaytech429@gmail.com>
2026-07-02 15:31:47 +0000

    Login V2 added

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -8,6 +8,7 @@ import '../screens/registration_screen.dart';
 import '../widgets/404_widget.dart';
 import '../screens/settings.dart';
 import '../screens/loading_screen.dart';
+import '../screens/login2.dart';
 
 
 
@@ -25,7 +26,7 @@ class AppRoutes {
 final GoRouter appRouter = GoRouter(
   initialLocation: AppRoutes.home,
   routes:[
-  GoRoute(path: AppRoutes.home, builder: (BuildContext context, GoRouterState state) => const SplashScreen()),
+  GoRoute(path: AppRoutes.home, builder: (BuildContext context, GoRouterState state) => const Login2()),
   GoRoute(path:AppRoutes.login, builder:(BuildContext context, GoRouterState state) => const LoginScreen()),
   GoRoute(path: AppRoutes.registration, builder: (BuildContext context, GoRouterState state) => Registration()),
   GoRoute(path: AppRoutes.homepage, builder: (BuildContext context, GoRouterState state)=>  HomeScreen()),
